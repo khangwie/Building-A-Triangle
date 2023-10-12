a37cb6d0bfa8ddca524ac561d33465e353f4ada6
Lenovo-User <Lenovo-User@DESKTOP-DGL558B>
2023-10-12 23:14:46 +0200

    Add project files.

---
+++ Building A Triangle/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Building_A_Triangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         Console.WriteLine("            1                    ");
+         Console.WriteLine("          1   1                   ");
+         Console.WriteLine("        1   2   1                  ");
+         Console.WriteLine("      1   3   3   1                  ");
+        Console.WriteLine("     1    4   6   4  1                ");
+            
+            Console.ReadLine();
+
+        }
+    }
+}
